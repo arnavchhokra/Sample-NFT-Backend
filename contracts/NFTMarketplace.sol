e5b6e287f6397b9d7e004d8cc1a7f4444abee46a
Arnav Chhokra <arnavchhokra@gmail.com>
2023-08-21 23:10:30 +0530

    updated Name

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract MusicNFTMarketplace is ERC721("DAppFi", "DAPP"), Ownable {
+contract NFTMarketplace is ERC721("Sample", "SMP"), Ownable {
     string public baseURI =
         "https://bafybeidhjjbjonyqcahuzlpt7sznmh4xrlbspa3gstop5o47l6gsiaffee.ipfs.nftstorage.link/";
     string public baseExtension = ".json";
